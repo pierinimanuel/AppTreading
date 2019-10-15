f6c2281532b61339df639025043f07a06de4dedf
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-15 09:47:33 +0200

    esercizio completato

--- App_MultiTreading/MainWindow.xaml.cs
+++ App_MultiTreading/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -49,6 +50,28 @@ namespace App_MultiTreading
         private void AggiornamentoInterfaccia()
         {
             Lbl_risultato.Content = "finito";
+        }
+
+        private void Btn_conta_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Task.Factory.StartNew(DoCount);
+        }
+
+        private void DoCount()
+        {
+            for (int i = 0; i <= 100000; i++)
+            {
+                for (int j = 0; j <= 100000; j++)
+                {
+                    Dispatcher.Invoke(()=>AggiornamentoInterfaccia(j));
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        private void AggiornamentoInterfaccia(int j)
+        {
+            Lbl_conta.Content = j.ToString();
         }
     }
 }
